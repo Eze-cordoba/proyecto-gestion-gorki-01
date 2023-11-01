d6e2debc3cc98116ae039367f1eebb38a5486fee
eze822 <ezecordoba4@gmail.com>
2023-11-01 08:22:14 -0600

    9 - 10 commit echo manejo de errores en controller

--- src/main/java/com/gorki/gorkiSpringBoot/controller/GorkiController.java
+++ src/main/java/com/gorki/gorkiSpringBoot/controller/GorkiController.java
@@ -13,6 +13,7 @@ import com.gorki.gorkiSpringBoot.services.UsuarioServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
@@ -50,77 +51,126 @@ public class GorkiController {
 
         for (DeporteReservable deporte : deportes) {
             for (Reserva reserva : deporte.getReservas()) {
-                if (reserva.getFechaInicio().isAfter(ahora)) {
+                if (reserva.getFechaInicio().isBefore(ahora)) {
                     reservaService.eliminarReserva(reserva.getId());
                 }
             }
         }
     }
+
     @Scheduled(cron = "0 0 * * SUN") // Ejecutar todos los domingos a las 00:00 (medianoche)
     public void actualizarReservasDeLaSemana() {
-        // Coloca aquí el código que deseas ejecutar todos los domingos
-    }
 
+        List<DeporteReservable> deportes = deporteReservableService.findAll();
+
+        for (DeporteReservable deporteReservable : deportes) {
+
+            LocalDateTime now = LocalDateTime.now();
+            // Si hoy es sábado (6) o domingo (7), ajusta la fecha al próximo lunes (1)
+            if (now.getDayOfWeek().getValue() >= 6) {
+                now = now.plusDays(8 - now.getDayOfWeek().getValue());
+            }
+
+            // Define el horario de inicio (10:00 AM) y fin (8:00 PM)
+            LocalDateTime inicio = LocalDateTime.of(now.toLocalDate(), LocalTime.of(10, 0));
+            LocalDateTime fin = LocalDateTime.of(now.toLocalDate(), LocalTime.of(20, 0));
+            LocalDateTime nextSaturday = now.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+
+            while (inicio.isBefore(nextSaturday)) {
+                // Agrega horarios disponibles de lunes a viernes
+                if (inicio.toLocalTime().isAfter(LocalTime.of(20, 0))) {
+                    inicio = inicio.plusHours(12); // Si es después de las 20:00, suma 12 horas
+                } else {
+                    HorarioDisponible reserva = new HorarioDisponible(inicio, inicio.plusHours(2));
+                    HorarioDisponible disponible = horariosDisponiblesService.crearHorarioDisponible(reserva);
+                    disponible.setDeporteReservable(deporteReservable);
+                    deporteReservable.getHorariosDisponibles().add(disponible);
+
+                    // Siguiente reserva comienza 2 horas después
+                    inicio = inicio.plusHours(2);
+                }
+
+            }
+            deporteReservableService.updateDeporteReservable(deporteReservable);
+
+
+        }
+
+    }
 
 
     @GetMapping("/obtenerDeportes")
     public ResponseEntity<?> obtenerDeportes() {
-
-        return ResponseEntity.ok(deporteReservableService.findAll());
-
+        try {
+            List<DeporteReservable> deportes = deporteReservableService.findAll();
+            return ResponseEntity.ok(deportes);
+        } catch (Exception e) {
+            String errorMessage = "Se produjo un error al obtener la lista de deportes.";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
 
     @PostMapping("/crearDeporte")
     public ResponseEntity<?> crearDeporte(@RequestBody DeporteReservable deporteRequest) {
 
-        DeporteReservable deporte = new DeporteReservable();
-        deporte.setNombre(deporteRequest.getNombre());
-        deporte.setDescripcion(deporteRequest.getDescripcion());
+        try {
+            DeporteReservable deporte = new DeporteReservable();
+            deporte.setNombre(deporteRequest.getNombre());
+            deporte.setDescripcion(deporteRequest.getDescripcion());
 
-        DeporteReservable deporteDb = deporteReservableService.crearDeporteReservable(deporte);
-        List<HorarioDisponible> horarioDisponibles = new ArrayList<>();
+            DeporteReservable deporteDb = deporteReservableService.crearDeporteReservable(deporte);
+            List<HorarioDisponible> horarioDisponibles = new ArrayList<>();
 
-        LocalDateTime now = LocalDateTime.now();
+            LocalDateTime now = LocalDateTime.now();
 
-        // Si hoy es sábado (6) o domingo (7), ajusta la fecha al próximo lunes (1)
-        if (now.getDayOfWeek().getValue() >= 6) {
-            now = now.plusDays(8 - now.getDayOfWeek().getValue());
-        }
-
-        // Define el horario de inicio (10:00 AM) y fin (8:00 PM)
-        LocalDateTime inicio = LocalDateTime.of(now.toLocalDate(), LocalTime.of(10, 0));
-        LocalDateTime fin = LocalDateTime.of(now.toLocalDate(), LocalTime.of(20, 0));
-        LocalDateTime nextSaturday = now.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
-        //ACA VOY A AGREGAR UN IF DE SI SE PASO DE LAS 8 DE LA NOCHE  SUMARLE 12 HORAS Y QUE SIGA CREANDO HORARIOS Y SI
-        while (inicio.isBefore(nextSaturday)) {
-            // Agrega horarios disponibles de lunes a viernes
-            if (inicio.toLocalTime().isAfter(LocalTime.of(20, 0))) {
-                inicio = inicio.plusHours(12); // Si es después de las 20:00, suma 12 horas
-            } else {
-                HorarioDisponible reserva = new HorarioDisponible(inicio, inicio.plusHours(2));
-                HorarioDisponible disponible = horariosDisponiblesService.crearHorarioDisponible(reserva);
-                disponible.setDeporteReservable(deporteDb);
-                deporteDb.getHorariosDisponibles().add(disponible);
-
-                // Siguiente reserva comienza 2 horas después
-                inicio = inicio.plusHours(2);
+            // Si hoy es sábado (6) o domingo (7), ajusta la fecha al próximo lunes (1)
+            if (now.getDayOfWeek().getValue() >= 6) {
+                now = now.plusDays(8 - now.getDayOfWeek().getValue());
             }
 
+            // Define el horario de inicio (10:00 AM) y fin (8:00 PM)
+            LocalDateTime inicio = LocalDateTime.of(now.toLocalDate(), LocalTime.of(10, 0));
+            LocalDateTime fin = LocalDateTime.of(now.toLocalDate(), LocalTime.of(20, 0));
+            LocalDateTime nextSaturday = now.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+
+            while (inicio.isBefore(nextSaturday)) {
+                // Agrega horarios disponibles de lunes a viernes
+                if (inicio.toLocalTime().isAfter(LocalTime.of(20, 0))) {
+                    inicio = inicio.plusHours(12); // Si es después de las 20:00, suma 12 horas
+                } else {
+                    HorarioDisponible reserva = new HorarioDisponible(inicio, inicio.plusHours(2));
+                    HorarioDisponible disponible = horariosDisponiblesService.crearHorarioDisponible(reserva);
+                    disponible.setDeporteReservable(deporteDb);
+                    deporteDb.getHorariosDisponibles().add(disponible);
+
+                    // Siguiente reserva comienza 2 horas después
+                    inicio = inicio.plusHours(2);
+                }
+
+            }
+
+            deporteReservableService.updateDeporteReservable(deporteDb);
+
+            return ResponseEntity.ok("deporteCreado");
+        } catch (Exception e) {
+
+            String errorMessage = "Se produjo un error al crear el deporte :" + deporteRequest.getNombre();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage + ",error:"+e.getCause());
+
         }
 
-        deporteReservableService.updateDeporteReservable(deporteDb);
-
-        System.out.println("deporteCreado");
-
-        return ResponseEntity.ok("afs");
     }
 
 
     @GetMapping("/obtenerDeporte/{idDeporte}")
     public ResponseEntity<?> obtenerDeporte(@PathVariable Long idDeporte) {
 
-
-        return ResponseEntity.ok(deporteReservableService.findDeporteReservableById(idDeporte));
+        try {
+            return ResponseEntity.ok(deporteReservableService.findDeporteReservableById(idDeporte));
+        } catch (Exception e) {
+            String message = "error al obtener el deporte";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message + ",error:"+e.getCause());
+        }
 
     }
 
@@ -128,24 +178,43 @@ public class GorkiController {
     @PostMapping("/crearUsuario")
     public ResponseEntity<?> crearUsuario(@RequestBody Usuario usuario) {
 
-        Usuario usuarioBd = usuarioService.crearUsuario(usuario);
-        return ResponseEntity.ok(usuarioBd);
+
+        try {
+            Usuario usuarioBd = usuarioService.crearUsuario(usuario);
+            return ResponseEntity.ok(usuarioBd);
+        } catch (Exception e) {
+            String message = "error al crear el usuario";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message + ",error:"+e.getCause());
+        }
 
     }
 
     @GetMapping("/verUsuario/{idUsuario}")
     public ResponseEntity<?> verUsuario(@PathVariable Long idUsuario) {
 
-        Usuario usuarioBd = usuarioService.findUsuario(idUsuario);
-        return ResponseEntity.ok(usuarioBd);
+        try {
+            Usuario usuarioBd = usuarioService.findUsuario(idUsuario);
+            return ResponseEntity.ok(usuarioBd);
+        } catch (Exception e) {
+            String message = "error al mostrar el usuario";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message + ",error:"+e.getCause());
+        }
 
     }
 
     @DeleteMapping("/eliminarUsuario/{idUsuario}")
     public ResponseEntity<?> eliminarUsuario(@PathVariable Long idUsuario) {
 
-        usuarioService.eliminarUsuario(idUsuario);
-        return ResponseEntity.ok("todoBien");
+        try {
+
+            usuarioService.eliminarUsuario(idUsuario);
+            return ResponseEntity.ok("usuario eliminado correctamente");
+
+
+        }catch (Exception e){
+            String message = "error al eliminar el usuario";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message + ",error:"+e.getCause());
+        }
 
     }
 
@@ -203,21 +272,22 @@ public class GorkiController {
                     }
 
                 } else {
-                    return ResponseEntity.ok("EL HORARIO YA EXPIRO");
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("el horario ya expiro");
                 }
 
             } catch (Exception e) {
                 System.out.println(e.getCause() + e.getMessage());
             }
 
-            return ResponseEntity.ok("Fecha procesada exitosamente");
+            return ResponseEntity.ok("reserva echa exitosamente");
 
 
         } catch (Exception e) {
-            System.out.println(e.getCause() + e.getMessage());
+           String error = "error al hacer reserva";
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error + ",error:"+e.getCause());
         }
 
-        return ResponseEntity.ok("Fecha procesada exitosamente");
+
 
     }
 
